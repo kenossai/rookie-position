b63ca0e009bcc23f9806af23e7624b06a45a63d1
Kenneth Ossai <kenossai1@gmail.com>
2024-06-15 19:19:07 +0100

    Create job-card.blade.php

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracast</div>
+    <div class="py-8 font-bold">
+        <h3>Web Developer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div class="">
+            <a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-slate-600 transition-colors duration-300">Programming</a>
+            <a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-slate-600 transition-colors duration-300">Design</a>
+            <a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-slate-600 transition-colors duration-300">Cloud</a>
+        </div>
+        <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1718460000&semt=sph" alt="" class="rounded-xl w-10">
+    </div>
+</div>
