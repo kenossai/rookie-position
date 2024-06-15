969b8d84a11d9c79cc024470cb0920981dd16b21
Kenneth Ossai <kenossai1@gmail.com>
2024-06-15 17:57:19 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-    Hello 
+    <section>
+        <h3>Top Jobs</h3>
+
+        <div class="p-4 bg-hite/5 rounded-xl flex flex-col text-center">
+                <div class="self-start text-sm">Laracast</div>
+                <div class="py-8 font-bold">
+                    <h3>Web Developer</h3>
+                    <p>Full Time - From $60,000</p>
+                </div>
+                <div class="">Laracast</div>
+        </div>
+    </section>
 </x-layout>
