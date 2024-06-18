6235b5d1a562b2817ffe28162345262fbceb67ec
Kenneth Ossai <kenossai1@gmail.com>
2024-06-18 17:56:03 +0100

    Update job-card.blade.php

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,8 +1,8 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 cursor-pointer">
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 cursor-pointer group transition-colors duration-300">
     <div class="self-start text-sm text-gray-400">Laracast</div>
     <div class="py-8 font-bold">
-        <h3 class="">Web Developer</h3>
-        <p>Full Time - From $60,000</p>
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Web Developer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div class="">
