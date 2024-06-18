5e9ccda4628ded9a1de757bef150f49f943db8f0
Kenneth Ossai <kenossai1@gmail.com>
2024-06-18 17:44:55 +0100

    Update section-heading.blade.php

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,4 +1,4 @@
-<div class="inline-flex items-center gap-x-2">
+<div class="inline-flex items-center gap-x-2 mt-3">
     <span class="w-2 h-2 bg-white inline-block"></span>
     <h3 class="font-bold text-lg">{{ $slot }}</h3>
 </div>
