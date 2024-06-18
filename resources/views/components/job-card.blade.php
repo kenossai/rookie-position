441fb14bee119f3cb35b46792efbf3a80ed1f93d
Kenneth Ossai <kenossai1@gmail.com>
2024-06-18 19:13:45 +0100

    Update job-card.blade.php

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,13 +1,15 @@
 <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 cursor-pointer group transition-colors duration-300">
     <div class="self-start text-sm text-gray-400">Laracast</div>
-    <div class="py-8 font-bold">
+    <div class="py-8">
         <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Web Developer</h3>
         <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
-        <div class="">
-            <x-tags>Programming</x-tags>
+        <div>
+            <x-tags size="small">Backend</x-tags>
+            <x-tags size="small">Frontend</x-tags>
+            <x-tags size="small">Manager</x-tags>
         </div>
-        <img src="https://d3jmn01ri1fzgl.cloudfront.net/photoadking/webp_thumbnail/panache-news-logo-design-template-cmowys738581e9.webp" alt="" class="rounded-xl w-20">
+        <img src="https://d3jmn01ri1fzgl.cloudfront.net/photoadking/webp_thumbnail/panache-news-logo-design-template-cmowys738581e9.webp" alt="" class="rounded-xl w-10">
     </div>
 </div>
